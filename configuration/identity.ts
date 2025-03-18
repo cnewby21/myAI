ffa683171a6674f340d0f744efcb5aed832fe487
cnewby21 <conoronewby1@gmail.com>
2025-03-17 20:53:52 -0400

    Change tone
    OG text: TONE

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Conor Newby`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `FinWise`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Educational and Approachable`;
 export const AI_ROLE: string = `[AI ROLE]`;
