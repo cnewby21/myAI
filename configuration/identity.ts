defe73034a0a96a094bf5859e55c4e137bfb6595
cnewby21 <conoronewby1@gmail.com>
2025-03-17 20:50:38 -0400

    Change AI name to FinWise
    OG text: AI_NAME

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Conor Newby`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `FinWise`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
