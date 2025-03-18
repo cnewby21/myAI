a79d940bb63941e4cda764dc45cc06e261d583e0
cnewby21 <conoronewby1@gmail.com>
2025-03-17 21:19:09 -0400

    Update message placeholder
    OG: [MESSAGE PLACEHOLDER]

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `FinWise - Smarter Money`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Ask me about saving, budgeting, or financial literacy!`;
 export const FOOTER_MESSAGE: string = `For Educational Purposes Only. Not Financial Advice`;
 export const CLEAR_BUTTON_TEXT: string = `Erase`;
 export const PAGE_TITLE: string = `FinWise`;
