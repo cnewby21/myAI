ed5bcce0c293098b85aa0c8c85dfa452f7514d3d
cnewby21 <conoronewby1@gmail.com>
2025-03-17 21:00:52 -0400

    Update AI role
    OG: AI ROLE

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `College Student`;
 
 export const AI_NAME: string = `FinWise`;
 export const AI_TONE: string = `Educational and Approachable`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_ROLE: string = `Financial Literacy Assistant`;
