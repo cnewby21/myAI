7941de0d2bb4de10341837dbb13449567e758605
cnewby21 <conoronewby1@gmail.com>
2025-03-17 20:58:26 -0400

    Update owner description
    OG: OWNER DESCRIPTION

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Conor Newby`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `College Student`;
 
 export const AI_NAME: string = `FinWise`;
 export const AI_TONE: string = `Educational and Approachable`;
