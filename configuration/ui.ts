8aba9d351ad0a1d33d030a4cc5dd8e6078047c00
cnewby21 <conoronewby1@gmail.com>
2025-03-17 20:39:57 -0400

    Change page tile to FinWise
    OG Text: PAGE_TILE

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `FinWise - Smarter Money`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `For Educational Purposes Only. Not Financial Advice`;
 export const CLEAR_BUTTON_TEXT: string = `Erase`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `FinWise`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
